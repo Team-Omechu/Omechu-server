a42486c4d6f396c091b0d389d03677b341c7bf12
hwijune <jhj030811@gmail.com>
2025-07-09 17:37:00 +0900

    swagger 및 prisma  초기 설정

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ import express from "express";
 import session from "express-session";
 import MySQLStore from "express-mysql-session";
 import { handleUserSignUp } from "./controllers/auth.controller.js";
-import swaggerJSDoc from "swagger-jsdoc";
-import swaggerUiExpress from "swagger-ui-express"
+import swaggerAutogen from "swagger-autogen";
+import swaggerUiExpress from "swagger-ui-express";
 
 dotenv.config();
 
@@ -25,10 +25,10 @@ const sessionStore = new MySQLSession({
 // 세션 미들웨어 등록
 app.use(
   session({
-    secret: process.env.SESSION_SECRET,  
+    secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
-    store: sessionStore,                  // 세션을 DB에 저장
+    store: sessionStore, // 세션을 DB에 저장
     cookie: {
       httpOnly: true,
       maxAge: 1000 * 60 * 60, // 1시간
@@ -36,30 +36,39 @@ app.use(
   })
 );
 // swagger 미들웨어 등록
-app.use("/docs",swaggerUiExpress.serve,swaggerUiExpress.setup({},{swaggerOptions:{
-  url:"/openapi.json"
-},}));
-app.get("/openapi.json",async(req,res,next)=>{
-  const options={
-    openapi:"3.0.0",
-    disableLogs:true,
-    writeOutputFile:false,
-  }
-  const outputFile="/dev/null";
-  const routes=["./src/index.js"]
-  const doc={
-    info:{
-      title:"Omechu",
-      description: "Umc 8th Omech 데모데이 프로젝트"
+app.use(
+  "/docs",
+  swaggerUiExpress.serve,
+  swaggerUiExpress.setup(
+    {},
+    {
+      swaggerOptions: {
+        url: "/openapi.json",
+      },
+    }
+  )
+);
+app.get("/openapi.json", async (req, res, next) => {
+  const options = {
+    openapi: "3.0.0",
+    disableLogs: true,
+    writeOutputFile: false,
+  };
+  const outputFile = "/dev/null";
+  const routes = ["./src/index.js"];
+  const doc = {
+    info: {
+      title: "Omechu",
+      description: "Umc 8th Omech 데모데이 프로젝트",
     },
-    host:"localhost:3000",
-  }
-  const result =await swaggerAutogen(options)(outputFile,routes,doc);
-  res.json(result ? result.data:null)
-})
+    host: "localhost:3000",
+  };
+  const result = await swaggerAutogen(options)(outputFile, routes, doc);
+  res.json(result ? result.data : null);
+});
 
 // 기타 미들웨어
-app.use(cors({origin:['http://localhost:3000']}));
+app.use(cors({ origin: ["http://localhost:3000"] }));
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
