657c3970c21deb81b7c66f350e76fa5434b96a4f
hwijune <jhj030811@gmail.com>
2025-07-09 20:09:44 +0900

    에러 미들웨어 설정

--- src/index.js
+++ src/index.js
@@ -17,6 +17,7 @@ app.use((req, res, next) => {
   res.success = (success) => {
     return res.json({ resultType: "SUCCESS", error: null, success });
   };
+
   res.error = ({ errorCode = "unknown", reason = null, data = null }) => {
     return res.json({
       resultType: "FAIL",
