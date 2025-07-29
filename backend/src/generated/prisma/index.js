6daad9a4e343b96b66b548d0b1b718b3a6dbe9ae
Ncs89 <shckshf01@naver.com>
2025-07-29 15:59:30 +0900

    package에서 npm run start 수정

--- backend/src/generated/prisma/index.js
+++ backend/src/generated/prisma/index.js
@@ -427,7 +427,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": "../../../.env",
+    "rootEnvPath": null,
     "schemaEnvPath": "../../../.env"
   },
   "relativePath": "../../../prisma",
