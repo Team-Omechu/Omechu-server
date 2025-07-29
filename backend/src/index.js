47df20262eb507a979d86f9147b5653aede0f32d
Ncs89 <shckshf01@naver.com>
2025-07-29 18:03:05 +0900

    변경사항 저장

--- backend/src/index.js
+++ backend/src/index.js
@@ -50,7 +50,6 @@ import { handleReportReview } from "./controllers/reportReveiw.controller.js";
 import { handleGetCoordinates } from "./controllers/getCoordinates.controller.js";
 import { handleInsertMukburim } from "./controllers/mukburim.controller.js";
 dotenv.config();
-
 const app = express();
 const port = process.env.PORT || 3000;
 
