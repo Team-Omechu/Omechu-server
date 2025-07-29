b2efcf7ef679f2cf880079edc9db97948a7fa56a
Ncs89 <shckshf01@naver.com>
2025-07-29 15:53:32 +0900

    generated에 제대로 안들어가서 삭제하고 다시 generate

--- backend/src/generated/prisma/index.js
+++ backend/src/generated/prisma/index.js
@@ -427,7 +427,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": null,
+    "rootEnvPath": "../../../.env",
     "schemaEnvPath": "../../../.env"
   },
   "relativePath": "../../../prisma",
