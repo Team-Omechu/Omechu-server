fbc91b73cb02c32bdb44cdae98877b0b8d3df965
hwijune <jhj030811@gmail.com>
2025-08-06 16:08:12 +0900

    .env 파일 수정

--- backend/src/utils/token.js
+++ backend/src/utils/token.js
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken";
 export const generateAccessToken = (payload) => {
   console.log(payload);
   return jwt.sign(payload, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRES_IN || "1h",
+    expiresIn: process.env.JWT_EXPIRES_IN || "1d",
   });
 };
 export const generateRefreshToken = (payload) => {
