4b94bac65633dce85aff0167cf19d8e29cf3510c
hwijune <jhj030811@gmail.com>
2025-08-07 14:48:42 +0900

    오류 처리 및 점검

--- backend/src/controllers/recommend.management.controller.js
+++ backend/src/controllers/recommend.management.controller.js
@@ -165,7 +165,7 @@ export const handleAddMenuToExcept = async (req, res, next) => {
               type: 'object',
               required: ['menuId'],
               properties: {
-                menuId: { type: 'string', example: 'menu_001', description: '추가할 메뉴의 고유 ID' }
+                menuId: { type: 'string', example: 35, description: '추가할 메뉴의 고유 ID' }
               }
             },
             {
@@ -180,7 +180,7 @@ export const handleAddMenuToExcept = async (req, res, next) => {
         examples: {
           menuIdExample: {
             summary: '메뉴 ID로 추가',
-            value: { menuId: 'menu_001' }
+            value: { menuId: 35 }
           },
           menuNameExample: {
             summary: '메뉴 이름으로 추가',
@@ -386,7 +386,7 @@ export const handleRemoveMenuExcept = async (req, res, next) => {
         examples: {
           menuIdExample: {
             summary: '메뉴 ID로 제거',
-            value: { menuId: 'menu_005' }
+            value: { menuId: 35 }
           },
           menuNameExample: {
             summary: '메뉴 이름으로 제거',
