79f91b13181f8d62a1d6dcc5f397eca25bc2ffa5
hwijune <jhj030811@gmail.com>
2025-08-07 19:40:12 +0900

    메뉴 자동완성 API 에러 핸들링

--- backend/src/services/suggestions.service.js
+++ backend/src/services/suggestions.service.js
@@ -1,9 +1,11 @@
 import { getData } from "../repositories/suggestions.repository.js";
-import { NoMenuData } from "../errors.js";
-export const getMenuData = async (data) => {
+import { NoMenuData, NoParams } from "../errors.js";
+export const getMenuData = async (data = null) => {
   const menuData = await getData(data);
   if (menuData.error === "NO_DATA") {
     throw new NoMenuData("해당 메뉴의 정보가 없습니다", data);
+  } else if (menuData.error === "NO_PARAMS") {
+    throw new NoParams("전달된 파라미터가 없습니다", data);
   } else {
     return menuData;
   }
