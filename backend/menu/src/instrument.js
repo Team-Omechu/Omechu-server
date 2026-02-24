17547bc62b21078296804ae33e4b46dfa3381144
hwijune <jhj030811@gmail.com>
2026-02-24 11:43:32 +0900

    도메인별 sentry 설정

---
+++ backend/menu/src/instrument.js
@@ -0,0 +1,12 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import * as Sentry from "@sentry/node";
+
+if (process.env.SENTRY_DSN) {
+  Sentry.init({
+    dsn: process.env.SENTRY_DSN,
+    sendDefaultPii: true,
+  });
+  Sentry.setTag("service", "menu_api");
+}
