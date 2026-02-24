e0f4925d8261bd5bf0a717d2c16297ad4b3c25b5
hwijune <jhj030811@gmail.com>
2026-02-24 14:18:59 +0900

    merge develop

--- backend/user/src/repositories/profile.repository.js
+++ backend/user/src/repositories/profile.repository.js
@@ -15,7 +15,6 @@ export const findUserProfile = async (userId) => {
 // 프로필 수정
 export const updateUserProfile = async (userId, data) => {
   const { prefer, allergy, ...userData } = data;
-
   return prisma.$transaction(async (tx) => {
     await tx.user.update({
       where: { id: userId },
@@ -29,7 +28,7 @@ export const updateUserProfile = async (userId, data) => {
       await tx.prefer.deleteMany({ where: { user_id: userId } });
       if (prefer.length) {
         await tx.prefer.createMany({
-          data: prefer.map(p => ({ user_id: userId, prefer: p })),
+          data: prefer.map((p) => ({ user_id: userId, prefer: p })),
         });
       }
     }
@@ -48,7 +47,7 @@ export const updateUserProfile = async (userId, data) => {
         }
 
         await tx.user_allergy.createMany({
-          data: allergyIds.map(a => ({
+          data: allergyIds.map((a) => ({
             user_id: userId,
             allergy_id: a.id,
           })),
